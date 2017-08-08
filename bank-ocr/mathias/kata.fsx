630d9336e3ee8d4a6cdf1fa51a2740d38fd8dab7
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2017-08-08 10:14:38 -0700

    Bank OCR: retrieve block at position i

--- bank-ocr/mathias/kata.fsx
+++ bank-ocr/mathias/kata.fsx
@@ -3,3 +3,19 @@ open System.IO
 
 let samplePath = Path.Combine (__SOURCE_DIRECTORY__,"../sample")
 let sample = File.ReadAllLines samplePath
+
+let prepare (lines:string[]) = 
+    Array2D.init 4 (3*9) (fun row col -> lines.[row].[col])
+    
+let prepared = prepare sample
+
+let width = 3
+let height = 4
+
+let blockAt (data:_[,]) position =
+    
+    let offset = width * position
+    Array2D.init height width (fun row col -> data.[row,col+offset])
+
+[ 0 .. 9 ]
+|> Seq.map (blockAt prepared)
