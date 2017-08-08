fd0c29930947156b1726fc7873b80d010204d67b
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2017-08-08 10:48:58 -0700

    Bank OCR: crappy printer

--- bank-ocr/mathias/kata.fsx
+++ bank-ocr/mathias/kata.fsx
@@ -24,6 +24,13 @@ let blockAt (size:BlockSize) (data:_[,]) position =
         size.Height size.Width 
         (fun row col -> data.[row, col + offset])
 
+let blockPrinter (size:BlockSize) (block:_[,]) =
+    printfn ""
+    block 
+    |> Array2D.iteri (fun row col x -> 
+        // linebreaks
+        if col % (size.Width) = 0 then printfn ""
+        printf "%A" x)
 
 let size = {
     Width = 3
@@ -32,10 +39,10 @@ let size = {
 
 let samplePath = Path.Combine (__SOURCE_DIRECTORY__,"../sample")
 let sample = File.ReadAllLines samplePath
-let prepared = prepare sample
+let prepared = prepare size 9 sample
 
 let blockReader = blockAt size prepared
 
-[ 0 .. 9 ]
+[ 0 .. 8 ]
 |> Seq.map blockReader
-
+|> Seq.iter (blockPrinter size)
